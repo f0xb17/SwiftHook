e3e2dfc4d4bd9d76fbbed7a82487343a2c7a6590
f0xb17 <102655383+f0xb17@users.noreply.github.com>
2025-01-03 19:23:30 +0100

    Added main()
    This method is used to execute the webhook request to discord. It
    prepares the JSON Data that is going to be send to Discord and even
    catches the errors.

--- Sources/main.swift
+++ Sources/main.swift
@@ -14,3 +14,44 @@ func createRequest(url: URL, body: Data) -> URLRequest {
 func message() -> String {
   return "Meddl Loide"
 }
+
+func main() {
+
+  let webhookURL = ""
+  guard let url = URL(string: webhookURL) else {
+    print("Invalid URL")
+    return
+  }
+
+  let content: [String: Any] = [
+    "content": message(),
+  ]
+  let jsonData = try! JSONSerialization.data(withJSONObject: content, options: [])
+  let semaphore = DispatchSemaphore(value: 0)
+  
+  let request = createRequest(url: url, body: jsonData)
+  let task = URLSession.shared.dataTask(with: request) {data, response, error in
+    guard let data = data, error == nil else {
+      print(error?.localizedDescription ?? "No data")
+      semaphore.signal()
+      return
+    }
+    let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
+    semaphore.signal()
+    if let responseJSON = responseJSON as? [String: Any] {
+      semaphore.signal()
+      print(responseJSON)
+    }
+    if let response = response as? HTTPURLResponse, response.statusCode == 204 {
+      print("Message sent successfully!")
+    } else {
+      print("Failed to send message")
+    }
+    semaphore.signal()
+  }
+  task.resume()
+  semaphore.wait()
+}
+
+main()
+
